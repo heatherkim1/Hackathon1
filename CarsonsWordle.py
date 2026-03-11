49d41c5a66ace5f08807287666151702254dca4b
Carttss <13945@edu.wheatlandhigh.org>
2026-03-11 16:21:08 +0000

    CarsonsWordle.py

--- CarsonsWordle.py
+++ CarsonsWordle.py
@@ -38,7 +38,21 @@ while True:
                 alltext = file.read()
                 words = list(map(str, alltext.split()))
                 #picking random word
-                word = random.choice(words)
+                wordle = random.choice(words)
+        elif language == "SPANISH":
+            #spanish word sleection process
+            print("You have chosen spanish!")
+            sleep(spacing)
+            with open("WordleWordsSpanish.txt", "r") as file:
+                alltext = file.read()
+                words = list(map(str, alltext.split()))
+                #picking random word
+                wordle = random.choice(words)
+        else:
+            print("Please enter a valid response (English/Spanish)")
+            language = input("Please select a language (English/Spanish) (not case sensitive)").upper().strip()
+            sleep(0.5)
+        
 
 
 
@@ -51,4 +65,44 @@ while True:
     else:
         print("Please enter a valid response (Y/N)")
         BeginInput = input("Are you ready to begin? (Y/N) (not case sensitive)").upper().strip()
+    def check_place(char_g, char_w, place):
+        if char_g == char_w:
+            print(place + " letter: right letter, right place!")
+    guess = input("Please enter your first guess! (5 letter word)").lower().strip()
+    #Checks to make sure its a 5 letter word.
+    while len(guess) != 5:
+        print("That was not a 5 letter word, please try again!")
+        guess = input("Please enter your first guess! (5 letter word)").lower().strip()
+    #a loop that repeats for each guess
+    for i in range(5):
+        #checks if guess is correct, if not, checks each letter and gives feedback.
+        if wordle == guess:
+            print("Congratulations! You guessed the word correctly!")
+            break
+        check_place(guess[0], wordle[0], "First")
+        if guess[0] == wordle[1] or guess[0] == wordle[2] or guess[0] == wordle[3] or guess[0] == wordle[4]:
+            print("First letter: right letter, wrong place!")
+        check_place(guess[1], wordle[1], "Second")
+        if guess[1] == wordle[0] or guess[1] == wordle[2] or guess[1] == wordle[3] or guess[1] == wordle[4]:
+            print("Second letter: right letter, wrong place!")
+        check_place(guess[2], wordle[2], "Third")
+        if guess[2] == wordle[0] or guess[2] == wordle[1] or guess[2] == wordle[3] or guess[2] == wordle[4]:
+            print("Third letter: right letter, wrong place!")
+        check_place(guess[3], wordle[3], "Fourth")
+        if guess[3] == wordle[0] or guess[3] == wordle[1] or guess[3] == wordle[2] or guess[3] == wordle[4]:
+            print("Fourth letter: right letter, wrong place!")
+        check_place(guess[4], wordle[4], "Fifth")
+        if guess[4] == wordle[0] or guess[4] == wordle[1] or guess[4] == wordle[2] or guess[4] == wordle[3]:
+            print("Fifth letter: right letter, wrong place!")
+        guess = input("please enter your next guess! (5 letter word)").lower().strip()
+        while len(guess) != 5:
+            print("That was not a 5 letter word, please try again!")
+            guess = input("Please enter your next guess! (5 letter word)").lower().strip()
+        if guess != wordle:
+            print("You have no more guesses, sadly the word was " + wordle + "!")
+            sleep(spacing)
+            print("beter luck next time!")
+            break
+
+
     
