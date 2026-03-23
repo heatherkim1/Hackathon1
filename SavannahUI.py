005b05204250f7ba9683b1f962d6d9907f5d20be
AppropriateUsername <13932@edu.wheatlandhigh.org>
2026-03-23 20:50:32 +0000

    done

--- SavannahUI.py
+++ SavannahUI.py
@@ -1,11 +1,11 @@
 
 main_menu = input("Welcome to our Hackathon project. What would you like to do?(View games, View records, or View game history logs. Note: please type as you see here.)")
 if main_menu == 'View games':
-    print("Connections: ")
-    print("Wordle: ")
-    print("Bloons: ")
-    print("Tic-Tac-Toe: ")
-    play_game = input('Which game would you like to play? (If you would like to return to the main menu type: Main menu)')
+    print("Connections: Connections is a popular daily word-grouping puzzle created by The New York Times in 2023. Players are given a grid of 16 words and must organize them into four groups of four that share a common, hidden theme.")
+    print("Wordle: Wordle is a popular daily online word game where players have six attempts to guess a secret five-letter word. After each guess, the letter tiles change color to show if they are correct (green), in the wrong place (yellow), or not in the word at all (gray). ")
+    print('Bloons: Bloons is a popular, long-running tower defense series by Ninja Kiwi where players place monkey towers along a path to pop incoming waves of colorful "bloons." The goal is to stop them from reaching the end using strategic tower placement, upgrades, hero abilities, and diverse monkey types to counter special bloon types.')
+    print("Tic-Tac-Toe: Tic-tac-toe is a classic two-player strategy game played on a 3x3 grid, where players alternate marking cells with 'X' or 'O'. The objective is to be the first to align three of their marks horizontally, vertically, or diagonally.")
+    play_game = input('Which game would you like to play?')
 
 if main_menu == 'View records':
     which_rec = input('Which game would you like to see the records for? (Bloons, Wordle, Connections, Tic-Tac-Toe)')
